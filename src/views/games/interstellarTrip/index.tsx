a8b6a367f288bbe73e730bfd630465bbf868eddb
Markovchaloven <2053492@brunel.ac.uk>
2021-09-21 12:55:31 +0800

    feat: fit pictures

--- src/views/games/interstellarTrip/index.tsx
+++ src/views/games/interstellarTrip/index.tsx
@@ -3,54 +3,20 @@ import { ReactElement, useRef, useEffect } from "react";
 import "../../../assets/styles/interstellarTrip.less"
 
 import {
-    StonesSrc, SpacecraftSrc, Spaceship,
-    BaseStone, Vector, Picture, BgcSrc
+    ISpaceship, IBaseStone, Vector, IPicture,
+    MoveItem
 } from "./type"
 
+import { StonesProperties, SpacecraftSrc, BgcSrc } from "./type"
+
 // 用于绘制画布的函数
-const fillPicture = (ctx: CanvasRenderingContext2D, body: Spaceship | Picture | BaseStone | BaseStone[]) => {
-    if (Array.isArray(body)) {
-        body.forEach(item => {
-            const img = new Image();
-            img.onload = function () {
-                ctx.drawImage(img, item.coordinate.x, item.coordinate.y, item.size.x, item.size.y)
-            }
-            img.src = item.src
-        })
-    } else {
-        const img = new Image();
-        img.onload = function () {
-            ctx.drawImage(img, body.coordinate.x, body.coordinate.y, body.size.x, body.size.y)
-        }
-        img.src = body.src
-    }
-}
+import { fillPicture, isColliding } from "./utils"
 
-// 用于判断陨石是否与飞船碰撞的函数
-const isColliding = (spaceship: Spaceship, stone: BaseStone) => {
-    const xdistance = spaceship.coordinate.x - stone.coordinate.x
-    const ydistance = spaceship.coordinate.y - stone.coordinate.y
-    if ((xdistance <= 0 && ydistance <= 0
-        && -xdistance <= spaceship.size.x
-        && -ydistance <= spaceship.size.y)
-        || (xdistance <= 0 && ydistance >= 0
-            && -xdistance <= spaceship.size.x
-            && ydistance <= stone.size.y)
-        || (xdistance >= 0 && ydistance <= 0
-            && xdistance <= stone.size.x
-            && -ydistance <= spaceship.size.y)
-        || (xdistance >= 0 && ydistance >= 0
-            && xdistance <= stone.size.x
-            && ydistance <= stone.size.y)) {
-        spaceship.destroyed = true
-    }
-}
+let Stones: MoveItem[]
 
-let Stones: BaseStone[]
+let player: MoveItem
 
-let player: Spaceship
-
-let bgc: Picture
+let bgc: MoveItem
 
 const FPS = 100;
 
@@ -78,30 +44,24 @@ const InterstellarTrip = (): ReactElement => {
         moveCoordX = e.targetTouches[0].clientX
         moveCoordY = e.targetTouches[0].clientY
 
-        let timer: NodeJS.Timer
+        // let moveStatus: boolean = false
 
-        let moveStatus: boolean = false
-
-        // timer = setInterval(() => {
-        //     player.velocity.x = 0
-        //     moveStatus = true
-        // }, 200)
-        if (coordX <= lastX + player.size.x + 100
-            && coordX >= lastX - 100
+        if (coordX <= lastX + player.size.x + 30
+            && coordX >= lastX - 30
             && coordY <= lastY + player.size.y + 50
             && coordY >= lastY - 50
         ) {
-
             if (Math.abs(moveCoordX - player.coordinate.x) >= 10) {
-                moveCoordX - player.coordinate.x > 0 ? player.velocity.x = 2 : player.velocity.x = -2
+                moveCoordX - player.coordinate.x > 0 ? player.changeSpeed({ x: 1, y: 0 }) : player.changeSpeed({ x: -1, y: 0 })
             } else {
-                player.velocity.x = 0
+                player.changeSpeed({ x: 0, y: 0 })
             }
         }
 
     }
 
     const spaceMoveEnd = () => {
+        moveCoordX = player.coordinate.x + player.size.x / 2
         player.velocity.x = 0
     }
 
@@ -111,40 +71,53 @@ const InterstellarTrip = (): ReactElement => {
         canvasRef.current!.setAttribute("height", height.toString());
         canvasRef.current!.setAttribute("width", width.toString());
         context = canvasRef.current?.getContext('2d')!;
-        Stones = Array.from({ length: 15 }, (v, i) => ({
-            coordinate: { x: width * (0.9 - Math.random()), y: -height * 2 / 15 * i },
-            size: { x: 40, y: 40 },
-            velocity: { x: 0, y: 1 },
-            src: StonesSrc[Math.floor(Math.random() * 15)]
-        }))
+        {/*算出比例，根据比例适配不同屏幕 */ }
+        const ratio = width / 375;
 
-        player = {
-            coordinate: { x: (width - 60) / 2, y: height - 100 },
-            size: { x: 60, y: 80 },
-            velocity: { x: 0, y: 20 },
+        StonesProperties.forEach((item) => {
+            item.coordinate.x *= ratio
+            item.coordinate.y = Math.floor(12 * Math.random()) * ((-height - 300) / 12)
+            item.size.x = item.size.x * ratio / 2
+            item.size.y = item.size.y * ratio / 2
+        })
+
+        Stones = Array.from({ length: 15 }, (_, i) => (
+            new MoveItem(StonesProperties[Math.floor(Math.random() * 17)])
+        ))
+
+        console.log(Stones);
+
+
+        player = new MoveItem({
+            coordinate: { x: (width - 60 * ratio) / 2, y: height - 100 * ratio },
+            size: { x: 60 * ratio, y: 80 * ratio },
+            velocity: { x: 0, y: 0 },
             destroyed: false,
             src: SpacecraftSrc
-        }
+        })
 
-        bgc = {
+        bgc = new MoveItem({
             coordinate: { x: 0, y: -width * 6.4 + height },
             size: { x: width, y: width * 6.4 },
-            velocity: { x: 0, y: width * 6.4 / (15 * FPS) },
-            src: BgcSrc
-        }
-
-        moveCoordX = player.coordinate.x
-        moveCoordY = player.coordinate.y
+            velocity: { x: 0, y: (width * 6.4 - height) / (15 * FPS) },
+            src: BgcSrc,
+        })
 
         setInterval(() => {
 
-            if (Math.abs(moveCoordX - player.coordinate.x) >= 10) {
-                moveCoordX - player.coordinate.x > 0 ? player.velocity.x = 2 : player.velocity.x = -2
-            } else {
-                player.velocity.x = 0
+            if (coordX <= lastX + player.size.x + 30
+                && coordX >= lastX - 30
+                && coordY <= lastY + player.size.y + 50
+                && coordY >= lastY - 50
+            ) {
+                if (Math.abs(moveCoordX - player.coordinate.x - player.size.x / 2) >= 10) {
+                    moveCoordX - player.coordinate.x > 0 ? player.changeSpeed({ x: 1, y: 0 }) : player.changeSpeed({ x: -1, y: 0 })
+                } else {
+                    player.changeSpeed({ x: 0, y: 0 })
+                }
             }
 
-            if (counter > 15 * FPS) {
+            if (counter > 12 * FPS) {
                 if (!context) return
                 context.clearRect(0, 0, width, height)
                 context.fillStyle = "pink"
@@ -152,18 +125,17 @@ const InterstellarTrip = (): ReactElement => {
                 context.fillText("挑战成功", 100, 250)
             } else if (!player.destroyed) {
                 if (!context) return
+                context.clearRect(0, 0, width, height)
                 counter++
-                // fillCanvas(context, player, "pink")
-                bgc.coordinate.y += bgc.velocity.y
-                Stones.forEach(item => {
-                    // item.coordinate.x += item.velocity.x
-                    item.coordinate.y += item.velocity.y
-                    // isColliding(player, item)
-                })
-                fillPicture(context, Stones)
                 fillPicture(context, bgc)
                 fillPicture(context, player)
-                // fillPicture(context, player)
+                fillPicture(context, Stones)
+                player.coordinate.x += player.velocity.x
+                bgc.coordinate.y += bgc.velocity.y
+                Stones.forEach(item => {
+                    item.changeSpeed({ x: 0, y: 1 })
+                    isColliding(player, item)
+                })
 
             } else {
                 if (!context) return
