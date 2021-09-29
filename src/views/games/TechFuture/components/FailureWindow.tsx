0a14cd913f39c26c107d0ff02af9c245732b5cab
lumingrui <lomirus@qq.com>
2021-09-29 13:37:02 +0800

    feat: add link to selection for some buttons

--- src/views/games/TechFuture/components/FailureWindow.tsx
+++ src/views/games/TechFuture/components/FailureWindow.tsx
@@ -2,13 +2,14 @@ import { ReactElement } from "react";
 
 import { playAgain, selectGame } from "../../../../assets/images/tech-future/images";
 
+import { Link } from "react-router-dom";
 
 const FailWindow = (): ReactElement => {
     return (
         <div className="fail-window">
             <div className="background">
                 <img className="play-again" src={playAgain.src} onClick={() => window.location.reload()}/>
-                <img className="select-game" src={selectGame.src} />
+                <Link to="/selection"><img className="select-game" src={selectGame.src} /></Link>
             </div>
         </div>
     )
--- src/views/games/TechFuture/components/SuccessWindow.tsx
+++ src/views/games/TechFuture/components/SuccessWindow.tsx
@@ -2,11 +2,12 @@ import { ReactElement } from "react";
 
 import { back } from "../../../../assets/images/tech-future/images";
 
+import { Link } from "react-router-dom";
 
 const SuccessWindow = (): ReactElement => {
     return <div className="success-window">
         <div className="background">
-            <img className="back" src={back.src} />
+            <Link to="selection"><img className="back" src={back.src} /></Link>
         </div>
     </div>
 }
