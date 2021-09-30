8c6ec40612b18007879d4bdfcb129bd506b7304a
ancient18 <2814013819@qq.com>
2021-09-30 22:17:53 +0800

    update

--- src/components/Share.tsx
+++ src/components/Share.tsx
@@ -1,82 +1,70 @@
 import { ReactElement, useEffect, useRef } from "react";
 import "../assets/styles/share.less";
-import {
-	Link
-} from "react-router-dom";
+import { Link } from "react-router-dom";
 
-import day1 from "../assets/images/share/day.png"
-import day2 from "../assets/images/share/day1.png"
-import code1 from "../assets/images/share/code.png"
-import back from "../assets/images/share/back.png"
-import juanImg from "../assets/images/share/juan.png"
+import day1 from "../assets/images/share/day.png";
+import day2 from "../assets/images/share/day1.png";
+import code1 from "../assets/images/share/code.png";
+import back from "../assets/images/share/back.png";
+import juanImg from "../assets/images/share/juan.png";
+import smallcode from "../assets/images/share/smallcode.png";
 
-const Share = (): ReactElement => {
-	let endRef: React.RefObject<HTMLDivElement> = useRef<HTMLDivElement>(null);
+const Share = (pro: any): ReactElement => {
+    let endRef: React.RefObject<HTMLDivElement> = useRef < HTMLDivElement > (null);
 
-	useEffect(() => {
-		let end = endRef.current;
-<<<<<<< HEAD
-		let day = end.querySelector(".day");
-		// let left = end.querySelector(".left");
-		// let right = end.querySelector(".right");
-		let img = end.querySelector(".img");
-		let code = img.querySelector("img");
-=======
-		let day = end?.querySelector(".day");
-		let left = end?.querySelector(".left");
-		let right = end?.querySelector(".right");
-		let img = end?.querySelector(".img");
-		let code = img?.querySelector("img");
+    useEffect(() => {
+        let end = endRef.current;
 
->>>>>>> 34c2083bfb06a79879dc1bbbf839622dbfabffea
-		let arrImg = [0, 1];
-		function randomSort(a: number, b: number) {
-			return Math.random() > 0.5 ? 1 : -1;
-		}
+        let day = end.querySelector(".day");
+        let img = end.querySelector(".img");
+        let code = img.querySelector("img");
+        let cha = end.querySelector(".cha");
 
-		arrImg.sort(randomSort);
+        // if(pro.flag==true){
+        // 	end.style.display="table";
+        // }
 
-		if (arrImg[0] === 0) {
-			day.style.backgroundImage = `url(${day1})`
+        cha.addEventListener("click", function () {
+            end.style.display = "none";
+        });
 
-		} else {
-			day.style.backgroundImage = `url(${day2})`;
-			day.style.height = "71.5vh";
-		}
+        let arrImg = [0, 1];
+        function randomSort(a: number, b: number) {
+            return Math.random() > 0.5 ? 1 : -1;
+        }
 
-		// right.addEventListener("click", function () {
-		// 	img.style.display = "table";
-		// });
+        arrImg.sort(randomSort);
 
-		img.addEventListener("click", function () {
-			img.style.display = "none";
-		});
+        if (arrImg[0] === 0) {
+            day.style.backgroundImage = `url(${day1})`;
+            cha.style.top = "9vw";
+        } else {
+            day.style.backgroundImage = `url(${day2})`;
+            day.style.height = "71.5vh";
+        }
 
-		code.style.marginTop =
-			document.body.clientHeight * 0.5 - document.body.clientWidth * 0.5 + "px";
-	}, []);
+        img.addEventListener("click", function () {
+            img.style.display = "none";
+        });
 
-	return (
-		<div className="end" ref={endRef}>
-			<div className="day">
-				<div className="juan">
-					<img src={juanImg} alt=""/>
-					<div className="code"></div>
-				</div>
-			</div>
-			{/* <div className="left">
-				<Link to="/selection">
-					<img src={back} alt="" />
-				</Link>
-			</div> */}
-			{/* <div className="right"></div> */}
+        code.style.marginTop =
+            document.body.clientHeight * 0.5 - document.body.clientWidth * 0.5 + "px";
+    }, []);
 
-
-			<div className="img">
-				<img src={code1} alt="" />
-			</div>
-		</div>
-	);
+    return (
+        <div className="end" ref={endRef}>
+            <div className="day">
+                <div className="cha"></div>
+                <div className="juan">
+                    <img src={smallcode} alt="" />
+                    {/* <div className="code"></div> */}
+                </div>
+            </div>
+            <div className="img">
+                <img src={code1} alt="" />
+            </div>
+        </div>
+    );
 };
 
 export default Share;
--- src/views/Selection/index.tsx
+++ src/views/Selection/index.tsx
@@ -1,34 +1,38 @@
-import { useState, ReactElement, useEffect,useRef } from "react";
+import { useState, ReactElement, useEffect, useRef } from "react";
 import Sentakushi from "./sentakushi";
 import "../../assets/styles/selection.less";
-import { API_URL } from "../../config"
-import Share from "../../components/Share"
+import { API_URL } from "../../config";
+import Share from "../../components/Share";
 
 type GameDones = {
-    "harvest": boolean,
-    "interstellar_trip": boolean,
-    "anti_epidemic": boolean,
-    "tech_future": boolean,
-}
+    harvest: boolean;
+    interstellar_trip: boolean;
+    anti_epidemic: boolean;
+    tech_future: boolean;
+};
 
 async function getState(): Promise<GameDones> {
     if (sessionStorage.getItem("stuID")) {
-        const res = await fetch(`${API_URL}/status?stu_number=${sessionStorage.getItem("stuID")}`)
-        const data = await res.json()
-        if (data.info !== "success") return {
-            "harvest": false,
-            "interstellar_trip": false,
-            "anti_epidemic": false,
-            "tech_future": false,
-        };
-        return data.data
-
+        const res = await fetch(
+            `${API_URL}/status?stu_number=${sessionStorage.getItem("stuID")}`
+        );
+        const data = await res.json();
+        if (data.info !== "success")
+            return {
+                harvest: false,
+                interstellar_trip: false,
+                anti_epidemic: false,
+                tech_future: false,
+            };
+        return data.data;
     } else {
         return {
-            "harvest": localStorage.getItem("harvest") ? true : false,
-            "interstellar_trip": localStorage.getItem("interstellar_trip") ? true : false,
-            "anti_epidemic": localStorage.getItem("anti_epidemic") ? true : false,
-            "tech_future": localStorage.getItem("tech_future") ? true : false,
+            harvest: localStorage.getItem("harvest") ? true : false,
+            interstellar_trip: localStorage.getItem("interstellar_trip")
+                ? true
+                : false,
+            anti_epidemic: localStorage.getItem("anti_epidemic") ? true : false,
+            tech_future: localStorage.getItem("tech_future") ? true : false,
         };
     }
 }
@@ -45,30 +49,39 @@ function getScore(dones: GameDones) {
 
 const Selection = (): ReactElement => {
     const [dones, setDones] = useState({
-        "harvest": false,
-        "interstellar_trip": false,
-        "anti_epidemic": false,
-        "tech_future": false,
-    })
-    
+        harvest: false,
+        interstellar_trip: false,
+        anti_epidemic: false,
+        tech_future: false,
+    });
+
+    const [obj, setObj] = useState({ flag: false });
 
     useEffect(() => {
-        getState().then(state => setDones(state))
-    }, [])
+        getState().then((state) => setDones(state));
+    }, []);
 
-<<<<<<< HEAD
+    // <<<<<<< HEAD
     useEffect(() => {
-        console.log(dones)
-    //    if(dones["harvest"]==true&&dones["interstellar_trip"]==true&&dones["anti_epidemic"]==true&&dones["tech_future"]==true){
-    //        if(localStorage["flag"]){
-    //         localStorage.setItem("flag","false")
-    //        }
-    //        else{
-    //         localStorage.setItem("flag","true")
-    //        }
-    //    }
+        console.log(dones);
+        //    if(dones["harvest"]==true&&dones["interstellar_trip"]==true&&dones["anti_epidemic"]==true&&dones["tech_future"]==true){
+        //        if(localStorage["flag"]){
+        //         localStorage.setItem("flag","false")
+        //        }
+        //        else{
+        //         localStorage.setItem("flag","true")
+        //        }
+        //    }
 
-    }, [dones])
+        if (
+            dones["harvest"] == true &&
+            dones["interstellar_trip"] == true &&
+            dones["anti_epidemic"] == true &&
+            dones["tech_future"] == true
+        ) {
+            setObj({ flag: true });
+        }
+    }, [dones]);
 
     // if(localStorage.flag==true){
     //        return (
@@ -86,14 +99,14 @@ const Selection = (): ReactElement => {
     //         </div>
     //     )
     // }else{
-        
+
     // }
 
-=======
->>>>>>> 34c2083bfb06a79879dc1bbbf839622dbfabffea
+    // =======
+    // >>>>>>> 34c2083bfb06a79879dc1bbbf839622dbfabffea
     return (
         <div className="selection">
-            <Share/>
+            <Share pro={obj} />
             <div className="content">
                 <div className="sentakushis">
                     <Sentakushi type="harvest" done={dones.harvest} />
@@ -104,9 +117,7 @@ const Selection = (): ReactElement => {
                 <p>当前总积分： {getScore(dones)}</p>
             </div>
         </div>
-    )
-
-
-}
+    );
+};
 
 export default Selection;
