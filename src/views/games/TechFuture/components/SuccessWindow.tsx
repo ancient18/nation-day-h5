bfea8f612ada2d5fcc3d3ee7f16f1c1af4f7c452
lumingrui <lomirus@qq.com>
2021-09-30 10:15:50 +0800

    fix: cannot update the scores

--- src/views/games/TechFuture/components/SuccessWindow.tsx
+++ src/views/games/TechFuture/components/SuccessWindow.tsx
@@ -1,10 +1,24 @@
-import { ReactElement } from "react";
+import { ReactElement, useEffect } from "react";
 
 import { back } from "../../../../assets/images/tech-future/images";
 
 import { Link } from "react-router-dom";
 
+import { API_URL } from "../../../../config"
+
 const SuccessWindow = (): ReactElement => {
+    useEffect(() => {
+        if (sessionStorage.getItem("stuID")) {
+            fetch(`${API_URL}/complete/tech_future`, {
+                method: "POST",
+                body: JSON.stringify({
+                    stu_number: sessionStorage.getItem("stuID")
+                })
+            })
+        } else {
+            localStorage.setItem("tech_future", "true")
+        }
+    }, [])
     return <div className="success-window">
         <div className="background">
             <Link to="/selection"><img className="back" src={back.src} /></Link>
