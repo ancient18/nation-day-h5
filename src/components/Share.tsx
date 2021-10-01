9ad4525f534b1cd3ceacb6b04c3bf90a46accf4e
ancient18 <2814013819@qq.com>
2021-10-01 12:27:26 +0800

    update

--- src/components/Share.tsx
+++ src/components/Share.tsx
@@ -2,12 +2,9 @@ import { ReactElement, useEffect, useRef } from "react";
 import "../assets/styles/share.less";
 import { Link } from "react-router-dom";
 
-import day1 from "../assets/images/share/day.png";
-import day2 from "../assets/images/share/day1.png";
-import code1 from "../assets/images/share/code.png";
-import back from "../assets/images/share/back.png";
-import juanImg from "../assets/images/share/juan.png";
-import smallcode from "../assets/images/share/smallcode.png";
+
+import background1 from "../assets/images/share/background1.png";
+import background2 from "../assets/images/share/background2.png";
 
 const Share = (fn, flag): ReactElement => {
     let endRef: React.RefObject<HTMLDivElement> = useRef < HTMLDivElement > (null);
@@ -15,22 +12,11 @@ const Share = (fn, flag): ReactElement => {
     useEffect(() => {
         let end = endRef.current;
 
-        let day = end.querySelector(".day");
-        let img = end.querySelector(".img");
-        let code = img.querySelector("img");
+        
+        let code = end.querySelector("img");
         let cha = end.querySelector(".cha");
-        // console.log(pro);
 
-        console.log(fn.pro);
-
-
-
-        // if(pro.click==true){
-        //     console.log(1);
-        // 	end.style.display="table";
-        // }
-
-        console.log(flag);
+        
 
         cha.addEventListener("click", function () {
             if (!fn.pro.flag) {
@@ -46,33 +32,23 @@ const Share = (fn, flag): ReactElement => {
         }
 
         arrImg.sort(randomSort);
+        
 
         if (arrImg[0] === 0) {
-            day.style.backgroundImage = `url(${day1})`;
-            cha.style.top = "9vw";
+            
+            code.src=background1
         } else {
-            day.style.backgroundImage = `url(${day2})`;
-            day.style.height = "71.5vh";
+            code.src=background2
         }
 
-        img.addEventListener("click", function () {
-            img.style.display = "none";
-        });
-
-        code.style.marginTop =
-            document.body.clientHeight * 0.5 - document.body.clientWidth * 0.5 + "px";
+       
     }, []);
 
     return (
         <div className="end" ref={endRef}>
             <div className="day">
+                <img src={background1} alt=""/>
                 <div className="cha"></div>
-                <div className="juan">
-                    <img src={smallcode} alt="" />
-                </div>
-            </div>
-            <div className="img">
-                <img src={code1} alt="" />
             </div>
         </div>
     );
