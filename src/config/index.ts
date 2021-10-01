3e56bc7a436ea509a021e824b52219e845800188
lumingrui <lomirus@qq.com>
2021-10-01 11:47:58 +0800

    chore: migrate api url

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,4 @@
 export const API_URL =
     process.env.NODE_ENV === 'production'
-        ? 'https://be-dev.redrock.cqupt.edu.cn/national-day'
+        ? 'https://be-prod.redrock.cqupt.edu.cn/national-day'
         : '/api';
